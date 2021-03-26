6f48c1280f8704deccffb6a613102afa4593448a
Muhammad Fahmi Hidayah <f.hidayah@pixilapps.com>
2021-03-26 22:20:23 +0700

    update generator cruud

--- generator_client.py
+++ generator_client.py
@@ -5,16 +5,18 @@ import json
 
 GENERATOR_CLIENT_LOG = 'Client Log : '
 
-FILE_LIST = ['admin', 'apps', 'forms', 'tables', 'urls', 'views', ]
 
-HTML_FILE_LIST = ['confirm_delete', 'detail', 'form', 'list']
+# FILE_LIST = ['admin', 'apps', 'forms', 'tables', 'urls', 'views', ]
+#
+# HTML_FILE_LIST = ['confirm_delete', 'detail', 'form', 'list']
 
 def log(value):
     print('{} {}'.format(GENERATOR_CLIENT_LOG, value))
 
 
 def read_file(file_name, parent_path: str = None):
-    with open(os.path.join(parent_path if parent_path else os.path.dirname(os.path.abspath(__file__)), file_name), "r") as f:
+    with open(os.path.join(parent_path if parent_path else os.path.dirname(os.path.abspath(__file__)), file_name),
+              "r") as f:
         template_string = ''
         for line in f.readlines():
             template_string += line
@@ -27,6 +29,7 @@ def write_string(path, file_name, value):
     py_file = open(os.path.join(path, file_name), 'wt')
     py_file.write(value)
     py_file.close()
+
 
 class TargetModel:
 
@@ -43,7 +46,7 @@ class TargetModel:
         self.template_dir = pathlib.Path(directory) / 'templates'
 
     def make_template_dir(self):
-        template_dir =  pathlib.Path(self.template_dir)
+        template_dir = pathlib.Path(self.template_dir)
         if not template_dir.exists():
             template_dir.mkdir()
 
@@ -53,6 +56,7 @@ class TargetModel:
 
     def get_full_template_dir(self):
         return pathlib.Path(self.template_dir) / self.app_dir
+
 
 class GeneratorClient:
 
@@ -96,16 +100,14 @@ class GeneratorClient:
             log('Exception : directory {} not found'.format(self.root_path))
 
     def write_target_model(self, target_model: TargetModel):
-        for file_item in FILE_LIST:
-            write_string(target_model.directory, '{}.py'.format(file_item), target_model.result[file_item])
+        for file_item in target_model.result['result_files']:
+            write_string(target_model.directory, '{}.py'.format(file_item['name']), file_item['value'])
 
         target_model.make_template_dir()
 
-        for file_item in HTML_FILE_LIST:
-            write_string(target_model.get_full_template_dir(), '{}_{}.html'.format(target_model.app_dir, file_item), target_model.result['templates'][file_item])
-
-
-
+        for file_item in target_model.result['template_files']:
+            write_string(target_model.get_full_template_dir(),
+                         '{}_{}.html'.format(target_model.app_dir, file_item['name']), file_item['value'])
 
     def write_model(self):
         for target_model in self.target_models:
@@ -115,22 +117,24 @@ class GeneratorClient:
     def generate_model(self):
         self.target_models = self.read_model()
         for target_model in self.target_models:
-            response = requests.post(self.url, data={'username': self.username, 'password': self.password, 'models': target_model.model_str})
+            response = requests.post(self.url, data={'username': self.username, 'password': self.password,
+                                                     'models': target_model.model_str})
             log(response.text)
             target_model.result = json.loads(response.text)
         self.write_model()
 
-def test():
 
+def test():
     # target_model = TargetModel()
     # target_model.set_app_dir("/Users/fahmi/Documents/Pycharm/generator_client/project/todo")
     # print(target_model.app_dir)
     # print(target_model.template_dir)
 
-    generator_client : GeneratorClient = GeneratorClient()
+    generator_client: GeneratorClient = GeneratorClient()
     generator_client.initial_configuration()
     # generator_client.read_model()
     generator_client.generate_model()
+
 
 if __name__ == '__main__':
     test()
